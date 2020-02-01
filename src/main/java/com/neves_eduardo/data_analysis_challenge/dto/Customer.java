9d03a96e653df32b4ce8b7a2e45a687b618c541b
Eduardo <eduardoneves858@gmail.com>
2020-02-01 13:15:35 -0300

    feat: adding dto pojos

---
+++ src/main/java/com/neves_eduardo/data_analysis_challenge/dto/Customer.java
@@ -0,0 +1,40 @@
+package com.neves_eduardo.data_analysis_challenge.dto;
+
+public class Customer {
+    private String cnpj;
+    private String name;
+    private String businessArea;
+
+    public Customer(String cnpj, String name, String businessArea) {
+        this.cnpj = cnpj;
+        this.name = name;
+        this.businessArea = businessArea;
+    }
+
+    public Customer() {
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBusinessArea() {
+        return businessArea;
+    }
+
+    public void setBusinessArea(String businessArea) {
+        this.businessArea = businessArea;
+    }
+}
---
+++ src/main/java/com/neves_eduardo/data_analysis_challenge/dto/Salesman.java
@@ -0,0 +1,40 @@
+package com.neves_eduardo.data_analysis_challenge.dto;
+
+public class Salesman {
+    private String cpf;
+    private String name;
+    private double salary;
+
+    public Salesman(String cpf, String name, double salary) {
+        this.cpf = cpf;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Salesman() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
