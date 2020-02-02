7d77a5c2995156ea79d3867394b9309c50a59b56
neves-eduardo <39205974+neves-eduardo@users.noreply.github.com>
2020-02-02 09:23:39 -0300

    Test/decoder test (#5)
    * test: adding basic tests to the decoder
    
    * refactor:removing dao dependency

---
+++ src/test/java/com/neves_eduardo/data_analysis_challenge/decoder/SalesReportFileDecoderTest.java
@@ -0,0 +1,80 @@
+package com.neves_eduardo.data_analysis_challenge.decoder;
+
+import com.neves_eduardo.data_analysis_challenge.dao.BatFileDAO;
+import com.neves_eduardo.data_analysis_challenge.dto.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.*;
+
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.junit.Assert.*;
+
+public class SalesReportFileDecoderTest {
+    private SalesReportFileDecoder salesReportFileDecoder = new SalesReportFileDecoder();
+    private List<String> file;
+    private static final double DELTA = 0.001;
+    private SalesReport salesReport;
+
+    @Before
+    public void init() {
+        file = new ArrayList<>();
+        file.add("001ç1234567891234çDiegoç50000");
+        file.add("001ç3245678865434çRenatoç40000.99");
+        file.add("002ç2345675434544345çJose da SilvaçRural");
+        file.add("002ç2345675433444345çEduardoPereiraçRural");
+        file.add("003ç10ç[1-10-100,2-30-2.50,3-40-3.10]çDiego");
+        file.add("003ç08ç[1-34-10,2-33-1.50,3-40-0.10]çRenato");
+        salesReport = salesReportFileDecoder.decodeFile(file);
+    }
+
+    @Test
+    public void salesmanDecoderShouldConvertTheLineIntoAnObjectTest() {
+        assertEquals(salesReport.getSalesmen().get(0).getClass(), Salesman.class);
+        assertEquals(salesReport.getSalesmen().get(1).getClass(), Salesman.class);
+        assertEquals("Diego", salesReport.getSalesmen().get(0).getName());
+        assertEquals("Renato", salesReport.getSalesmen().get(1).getName());
+        assertEquals("1234567891234", salesReport.getSalesmen().get(0).getCpf());
+        assertEquals("3245678865434", salesReport.getSalesmen().get(1).getCpf());
+        assertEquals(50000, salesReport.getSalesmen().get(0).getSalary(), DELTA);
+        assertEquals(40000.99, salesReport.getSalesmen().get(1).getSalary(), DELTA);
+
+    }
+
+    @Test
+    public void customerDecoderShouldConvertTheLineIntoAnObjectTest() {
+        assertEquals(salesReport.getCustomers().get(0).getClass(), Customer.class);
+        assertEquals(salesReport.getCustomers().get(1).getClass(), Customer.class);
+        assertEquals("Jose da Silva", salesReport.getCustomers().get(0).getName());
+        assertEquals("EduardoPereira", salesReport.getCustomers().get(1).getName());
+        assertEquals("2345675434544345", salesReport.getCustomers().get(0).getCnpj());
+        assertEquals("2345675433444345", salesReport.getCustomers().get(1).getCnpj());
+
+
+    }
+
+    @Test
+    public void saleDecoderShouldConvertTheLineIntoAnObjectTest() {
+        assertEquals(salesReport.getSales().get(0).getClass(), Sale.class);
+        assertEquals(salesReport.getSales().get(1).getClass(), Sale.class);
+        assertEquals(10, salesReport.getSales().get(0).getSaleId());
+        assertEquals(8, salesReport.getSales().get(1).getSaleId());
+        assertEquals("2345675434544345", salesReport.getCustomers().get(0).getCnpj());
+        assertEquals("2345675433444345", salesReport.getCustomers().get(1).getCnpj());
+        assertEquals("Diego", salesReport.getSales().get(0).getSalesmanName());
+        assertEquals("Renato", salesReport.getSales().get(1).getSalesmanName());
+        assertThat(salesReport.getSales().get(0).getItems().get(0), samePropertyValuesAs(new Item(1, 10, 100)));
+        assertThat(salesReport.getSales().get(0).getItems().get(1), samePropertyValuesAs(new Item(2, 30, 2.50)));
+        assertThat(salesReport.getSales().get(0).getItems().get(2), samePropertyValuesAs(new Item(3, 40, 3.10)));
+        assertThat(salesReport.getSales().get(1).getItems().get(0), samePropertyValuesAs(new Item(1, 34, 10)));
+        assertThat(salesReport.getSales().get(1).getItems().get(1), samePropertyValuesAs(new Item(2, 33, 1.50)));
+        assertThat(salesReport.getSales().get(1).getItems().get(2), samePropertyValuesAs(new Item(3, 40, 0.10)));
+
+
+    }
+}
