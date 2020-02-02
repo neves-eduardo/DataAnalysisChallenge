7bf2e0ff97fa1f99ca9ae65571951bd24bf156c7
Eduardo <eduardoneves858@gmail.com>
2020-02-02 01:12:42 -0300

    refactor: removing IO exception throw from FileDecoderInterface

--- src/main/java/com/neves_eduardo/data_analysis_challenge/decoder/FileDecoder.java
+++ src/main/java/com/neves_eduardo/data_analysis_challenge/decoder/FileDecoder.java
@@ -7,5 +7,5 @@ import java.nio.file.Path;
 import java.util.List;
 
 public interface FileDecoder {
-    SalesReport decodeFile(List<String> lines) throws IOException;
+    SalesReport decodeFile(List<String> lines);
 }
